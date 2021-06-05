3cbb388af1097b004f3642d70c9bf3e81493b1a0
umar <umarkhan2025@gmail.com>
2021-06-05 19:19:59 +0530

    Files Modified

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
-const fileUpload = require('express-fileupload')
-
-const expHbs = require("express-handlebars")
-
+require('dotenv').config();
+  
 const express = require('express')
-
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+const homeRouter = require('./routes/home')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-app.use(fileUpload())
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use(express.urlencoded({ extended:true }));
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+app.use('/home', homeRouter)
 
-const fs = require('fs');
-
-// const users = require('./database/users.json')
-
-const cookiePraser = require('cookie-parser')
-
-app.use(cookiePraser())
-
-app.engine('hbs', expHbs({extname:'hbs'}))
-
-app.set('view engine', 'hbs')
+app.listen(process.env.PORT || 3000)
 
 
-
-
-
-
-const authRouter = require('./routes/auth')
-
-app.use('/', authRouter)
-
-
-app.listen(5000, () => {
-    console.log('server started')
-})
- 
