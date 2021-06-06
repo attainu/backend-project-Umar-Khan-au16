cb5b4f76f466d925e845118a88554baed6dd5269
umar <umarkhan2025@gmail.com>
2021-06-06 23:15:49 +0530

    Updated files

---
+++ routes/cloudinary.js
@@ -0,0 +1,10 @@
+// const cloudinary = require('cloudinary')
+
+// cloudinary.config({
+//   cloud_name: 'dibwo3lec',
+//   api_key: '567557441234553',
+//   api_secret: 'evoUU-UokRkC5S-Z5yoxLqUpW0g',
+//   API_Environment_variable: 'CLOUDINARY_URL://567557441234553:evoUU-UokRkC5S-Z5yoxLqUpW0g@dibwo3lec'
+// })
+
+// // module.exports = cloudinary
